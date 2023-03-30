5900630e777ea45bdaafeeb18240adedbaf95e07
Roberto Moreno <rob.moreno@hotmail.com>
2023-03-30 17:27:56 -0600

    I Messed up

--- src/chapter7/daysOfWeek.java
+++ src/chapter7/daysOfWeek.java
@@ -1,2 +1,30 @@
-package chapter7;public class daysOfWeek {
+package chapter7;
+
+import java.util.Scanner;
+
+public class daysOfWeek {
+
+   private static String [] days = new String[] {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
+   static Scanner scanner = new Scanner(System.in);
+
+    public static String getDay(int dayNumber)
+    {
+        for(int i=0; i<=days.length; i++)
+        {
+            if(dayNumber == i)
+            {
+                System.out.println(days[dayNumber - 1]);
+            }
+        }
+
+        return days[dayNumber - 1];
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Enter the Number of the day of the week you want to see: ");
+        int number = scanner.nextInt();
+        getDay(number);
+    }
+
 }
